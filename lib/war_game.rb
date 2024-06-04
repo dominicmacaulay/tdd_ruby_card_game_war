32a997352b0b0fffd2905b45341cb958d45a93d7
dominicmacaulay <dominic.62502@gmail.com>
2024-06-04 13:46:48 -0400

    Change #initialize in WarGame to accept Parameters
    Because of the Server/Socket additions that will be made, it appears
    as though the server will need to create a new player to associate
    with each client. Therefore, it will need to create a game with those
    players already made. The parameters in the WarGame class have new
    players as default values.

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -3,9 +3,9 @@ require_relative 'card_deck'
 class WarGame
     attr_reader :player1, :player2, :deck, :deck_length
     attr_accessor :winner
-    def initialize
-        @player1 = Player.new("Player 1")
-        @player2 = Player.new("Player 2")
+    def initialize(player1 = Player.new("Player 1"), player2 = Player.new("Player 2"))
+        @player1 = player1
+        @player2 = player2
         @deck = CardDeck.new()
         @deck_length = deck.cards_left
         @winner = nil
